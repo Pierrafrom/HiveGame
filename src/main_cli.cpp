f3d37042ac1a7b76cd651e026a760da8e898e6b8
pierre fro <pierrefromont@outlook.fr>
2024-12-05 17:48:47 +0100

    add the dual build configuartion

---
+++ src/main_cli.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World! This is the CLI version of HiveGame." << std::endl;
+    return 0;
+}
---
+++ src/main_gui.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include <QLabel>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    QLabel label("Hello, World! This is the GUI version of HiveGame.");
+    label.setAlignment(Qt::AlignCenter);
+    label.resize(400, 200);
+    label.show();
+
+    return QApplication::exec();
+}
