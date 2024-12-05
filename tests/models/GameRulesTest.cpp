1996c218c8b7c8165233061c07e7dd3b23cb5409
pierre fro <pierrefromont@outlook.fr>
2024-12-05 18:00:38 +0100

    init test class for game and gameRules

---
+++ tests/models/GameRulesTest.cpp
@@ -0,0 +1,24 @@
+// GameRulesTest.cpp
+#include <gtest/gtest.h>
+
+
+namespace hive::models {
+    class GameRulesTest : public testing::Test {
+    protected:
+        // TODO: declare any variables you need here
+
+        // Setup method for initializing the player
+        void SetUp() override {
+            //TODO : initialize any variables you need here
+            // every test will run this function before executing
+        }
+
+        // TearDown method for cleaning up
+        void TearDown() override {
+            //TODO : clean up any variables you need here
+            // every test will run this function after executing
+        }
+    };
+
+    // TODO: add your test cases here
+}
