f6c5792e7ee6f25569d4b054266a1167ff30013e
volkoffff <robin.lanfranchi@etudiant.univ-reims.fr>
2024-12-20 11:16:14 +0100

    feat(game) push

--- src/models/strategies/QueenBeeMoveStrategy.cpp
+++ src/models/strategies/QueenBeeMoveStrategy.cpp
@@ -17,7 +17,7 @@ namespace hive::models::strategies {
         for (const auto &direction: enums::getAllDirections()) {
             Hex neighbor{0, 0, 0}; // O,0,0 is a placeholder to create a hex object
             try {
-                neighbor = board.neighbor(neighbor, direction);
+                neighbor = board.neighbor(position, direction);
             } catch (const std::out_of_range &e) {
                 continue;
             } catch (const std::exception &e) {
@@ -25,8 +25,8 @@ namespace hive::models::strategies {
             }
             const bool occupied = board.isOccupied(neighbor);
             const bool canSlice = board.canSliceBetween(position, direction);
-            const bool connected = isHiveConnectedAfterMove(board, position, neighbor);
-            if (!occupied && canSlice && connected) {
+            //const bool connected = isHiveConnectedAfterMove(board, position, neighbor);
+            if (!occupied && canSlice) {
 
                 possibleMoves.push_back(neighbor);
 
